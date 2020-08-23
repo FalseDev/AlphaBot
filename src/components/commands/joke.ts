3a33264b5b42ffc5ff7d36fbcfe64f5b6f775411
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2020-08-23 17:42:08 +0530

    Joke file included

---
+++ src/components/commands/joke.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { TelegrafContext } from "telegraf/typings/context";
+
+interface Joke {
+  type: "twopart" | "single";
+  joke?: string;
+  setup?: string;
+  delivery?: string;
+}
+
+interface JokesData {
+  error: Boolean;
+  jokes: Joke[];
+  amount: number;
+}
+
+const joke = async (context: TelegrafContext) => {
+  const sendJoke = (joke: Joke) => {
+    if (joke.type === "single") {
+      return context.reply(joke.joke!);
+    }
+    context.reply(`${joke.setup!}\n\n${joke.delivery!}`);
+  };
+
+  let numberOfJokes = 1;
+  const comps = context.message!.text!.split(" ");
+  if (comps.length > 1) numberOfJokes = +comps[1];
+  if (Number.isNaN(numberOfJokes) || numberOfJokes < 1)
+    return context.reply("Please provide a proper number");
+
+  const jokeResponse = await axios.get(
+    "https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist",
+    { params: { amount: numberOfJokes }, data: "json" }
+  );
+  if (numberOfJokes > 1) {
+    const jokeData = <JokesData>jokeResponse.data;
+    const jokes = jokeData.jokes;
+
+    jokes.map(sendJoke);
+  } else {
+    const joke = <Joke>jokeResponse.data;
+    sendJoke(joke);
+  }
+  if (numberOfJokes > 10) {
+    context.reply("Note: you can get only upto 10 jokes at once :)");
+  }
+};
+
+export default joke;
